c49715e8620c4342330566ac4680168dd62c9d69
whatev <jamiesamglasson@gmail.com>
2021-06-12 04:47:13 -0400

    add runIfBool function

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+clear
 packadgeManager="apt-get"
 
+runIfBool(){
+    read -p "$1 y/N: " response
+    if [ $response == "y" ]; then $2; fi
+}
+
 installApp(){
-  sudo apt install $1
+    runIfBool "install $1?" 'sudo apt install $1'
 }
 
 update(){
-   sudo apt update
+    sudo apt update
 }
 
 #apps
@@ -21,23 +27,25 @@ installApp code
 
 echo "--------------------INSTALLING GNOME SHELL EXTENSIONS-----------------------"
 installApp gnome-shell-extension-gsconnect
-installApp gnome-shell-extension-hide-activities
+# installApp gnome-shell-extension-hide-activities # does not work
 
 #flutter (code from: https://flutter.dev/docs/get-started/install/linux#additional-linux-requirements)
-read -p "install flutter? Y/n: " installingFlutter
-if installingFlutter == "Y" {
-sudo snap install flutter --classic
-installApp clang cmake ninja-build pkg-config libgtk-3-dev
-flutter config --enable-linux-desktop}
+runIfBool "install flutter?" '
+  sudo snap install flutter --classic
+  installApp clang cmake ninja-build pkg-config libgtk-3-dev
+'
 
 #thing
-
-codeToInstallThemes = $[
-    () => {        
+bitarray>codeToInstallThemes = (
+    ["Zorin"] = '
         git clone https://github.com/ZorinOS/zorin-desktop-themes.git
         sudo cp -r zorin-desktop-themes/Z\* /usr/share/themes
-    }
+    '
 ]
+bitarray>select themeToInstall in !codeToInstallThemes[*]
+do
+    codeToInstallThemes["$themeToInstall"]
+done
 
 read -p "select theme \n 0. zorin theme \n" themeNumber
 codeToInstallThemes[int themeNumber]()
